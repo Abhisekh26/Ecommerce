43549310f893fecbd459af5fed857546edae3d8a
Abhisekh26 <142305216+Abhisekh26@users.noreply.github.com>
2024-02-13 18:19:26 +0530

    Add files via upload
    Adding dynamic cart

--- App.js
+++ App.js
@@ -1,98 +1,71 @@
 import { useState } from "react";
-import Header from "./Components/Headerfolder/Header" ;
+import Header from "./Components/Headerfolder/Header";
 import Body from "./Components/BodyFolder/Body";
 import Cartdisplay from "./Components/Cart/Cartdisplay";
 function App() {
-  const[displaycart,setDisplaycart]=useState(false)
-  
+  const [displaycart, setDisplaycart] = useState(false);
+  const [cart, setCart] = useState([]);
+
   var productsArr = [
     {
       title: "Colors",
-       price: 100,
-        imageUrl:
+      price: 100,
+      imageUrl:
         "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
     },
     {
       title: "Black and white Colors",
-       price: 50,
-        imageUrl:
+      price: 50,
+      imageUrl:
         "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
     },
     {
       title: "Yellow and Black Colors",
-       price: 70,
-        imageUrl:
+      price: 70,
+      imageUrl:
         "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
     },
-      {
+    {
       title: "Blue Color",
       price: 100,
-       imageUrl:
+      imageUrl:
         "https://prasadyash2411.github.io/ecom-website/img/Album%204.png",
-    }
+    },
   ];
 
-  var cartElements = [
+  function showcart() {
+    setDisplaycart(true);
+  }
 
-    {
-    
-    title: 'Colors',
-    
-    price: 100,
-    
-    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
-    
-    quantity: 2,
-    
-    },
-    
-    {
-    
-    title: 'Black and white Colors',
-    
-    price: 50,
-    
-    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
-    
-    quantity: 3,
-    
-    },
-    
-    {
-    
-    title: 'Yellow and Black Colors',
-    
-    price: 70,
-    
-    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
-    
-    quantity: 1,
-    
+  function addcart(ite) {
+    var newcart = cart.find((item) => item.title === ite.title);
+      if (!newcart) {
+      setCart([...cart, { ...ite, quantity: 1 }]);
+    } else {
+      setCart(
+        cart.map((item) =>
+          item.title === ite.title
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        )
+      );
     }
-    
-    ]
-    const[cart,setCart]= useState(cartElements)
-   
-    function showcart(){
-      setDisplaycart(true)
-    }
-    function deletecart(ite){
-      var newcart=cart.filter(cart=>cart.title!==ite.title)
-      setCart(newcart)
-      
-    }
+  }
+  // console.log(cart)
+  function deletecart(ite) {
+    var newcart = cart.filter((cart) => cart.title !== ite.title);
+    setCart(newcart);
+  }
 
   return (
     <div>
-      <Header data={cartElements}
-       showcart={showcart}
-       ></Header>
-      <Body data={productsArr}></Body>
-      {displaycart  &&
-      <Cartdisplay data={cart}
-        deletecart={deletecart}
-      ></Cartdisplay>
-}
+      <Header data={cart} showcart={showcart}></Header>
+
+      <Body data={productsArr} addcart={addcart}></Body>
+
+      {displaycart && (
+        <Cartdisplay data={cart} deletecart={deletecart}></Cartdisplay>
+      )}
     </div>
   );
 }
