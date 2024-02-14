8bd68b362a46d8766eaa2163377db8efd99725f8
Abhisekh26 <142305216+Abhisekh26@users.noreply.github.com>
2024-02-14 19:43:43 +0530

    Add files via upload
    routing about

--- App.js
+++ App.js
@@ -1,73 +1,25 @@
-import { useState } from "react";
-import Header from "./Components/Headerfolder/Header";
-import Body from "./Components/BodyFolder/Body";
-import Cartdisplay from "./Components/Cart/Cartdisplay";
+import Main from "./Main";
+import About from "./Components/About/About";
+import { createBrowserRouter ,RouterProvider} from "react-router-dom";
+
+
+const router=createBrowserRouter([{
+  path:'/about',element:<About/>},
+{path:"/",element:<Main/>}
+])
+
+
 function App() {
-  const [displaycart, setDisplaycart] = useState(false);
-  const [cart, setCart] = useState([]);
+  return(
+    <>
+    <RouterProvider router={router}></RouterProvider>
+    
+    </>
+    
+  )
 
-  var productsArr = [
-    {
-      title: "Colors",
-      price: 100,
-      imageUrl:
-        "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
-    },
-    {
-      title: "Black and white Colors",
-      price: 50,
-      imageUrl:
-        "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
-    },
-    {
-      title: "Yellow and Black Colors",
-      price: 70,
-      imageUrl:
-        "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
-    },
-    {
-      title: "Blue Color",
-      price: 100,
-      imageUrl:
-        "https://prasadyash2411.github.io/ecom-website/img/Album%204.png",
-    },
-  ];
-
-  function showcart() {
-    setDisplaycart(true);
-  }
-
-  function addcart(ite) {
-    var newcart = cart.find((item) => item.title === ite.title);
-      if (!newcart) {
-      setCart([...cart, { ...ite, quantity: 1 }]);
-    } else {
-      setCart(
-        cart.map((item) =>
-          item.title === ite.title
-            ? { ...item, quantity: item.quantity + 1 }
-            : item
-        )
-      );
-    }
-  }
-  // console.log(cart)
-  function deletecart(ite) {
-    var newcart = cart.filter((cart) => cart.title !== ite.title);
-    setCart(newcart);
-  }
-
-  return (
-    <div>
-      <Header data={cart} showcart={showcart}></Header>
-
-      <Body data={productsArr} addcart={addcart}></Body>
-
-      {displaycart && (
-        <Cartdisplay data={cart} deletecart={deletecart}></Cartdisplay>
-      )}
-    </div>
-  );
+  
+  
 }
 
 export default App;
---
+++ Main.js
@@ -0,0 +1,78 @@
+import { useState } from "react";
+// import { createBrowserRouter ,RouterProvider} from "react-router-dom";
+import Header from "./Components/Headerfolder/Header";
+import Body from "./Components/BodyFolder/Body";
+import Cartdisplay from "./Components/Cart/Cartdisplay";
+// import About from "./Components/About/About";
+function Main(){
+const [displaycart, setDisplaycart] = useState(false);
+
+const [cart, setCart] = useState([]);
+
+var productsArr = [
+  {
+    title: "Colors",
+    price: 100,
+    imageUrl:
+      "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
+  },
+  {
+    title: "Black and white Colors",
+    price: 50,
+    imageUrl:
+      "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
+  },
+  {
+    title: "Yellow and Black Colors",
+    price: 70,
+    imageUrl:
+      "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
+  },
+  {
+    title: "Blue Color",
+    price: 100,
+    imageUrl:
+      "https://prasadyash2411.github.io/ecom-website/img/Album%204.png",
+  },
+];
+
+function showcart() {
+  setDisplaycart (true);
+}
+
+function addcart(ite) {
+  var newcart = cart.find((item) => item.title === ite.title);
+    if (!newcart) {
+    setCart([...cart, { ...ite, quantity: 1 }]);
+  } else {
+    setCart(
+      cart.map((item) =>
+        item.title === ite.title
+          ? { ...item, quantity: item.quantity + 1 }
+          : item
+      )
+    );
+  }
+}
+
+function deletecart(ite) { 
+  var newcart = cart.filter((cart) => cart.title !== ite.title);
+  setCart(newcart);
+}
+
+return (
+  <>
+  {/* <RouterProvider router={router}></RouterProvider>  */}
+  <div>
+    <Header data={cart} showcart={showcart}></Header>
+    <Body data={productsArr} addcart={addcart}></Body>
+     {displaycart && (
+      <Cartdisplay data={cart} deletecart={deletecart}></Cartdisplay>
+    )}
+    </div>
+
+  </>
+
+    )
+}
+export default Main;
